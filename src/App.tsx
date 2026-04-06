866553b42e4581229be04a48a5ffbd75e837a899
sfloody1 <sfloody1@jhu.edu>
2026-04-05 22:34:58 -0400

    dashboard-04: Create layout and style for the Covid Dashboard App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button";
+import SelectCountry from "@/components/select-country";
+import DisplayStatistics from "@/components/display-statistics";
 
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Button>Click me</Button>
+    <div className="flex flex-col justify-between items-center min-h-screen max-w-4xl m-auto py-10">
+      <SelectCountry />
+      <DisplayStatistics />
     </div>
   );
 }
